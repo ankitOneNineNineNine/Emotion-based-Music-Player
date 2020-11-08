c9d5bb1964fe9f129eb6ac69a8905797883359b5
ankitOneNineNineNine <pradhanankit12@gmail.com>
2020-11-08 08:34:44 +0545

    some changes

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({ limit:'50mb', extended: false }));
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 // app.use('/music', express.static(path.join(__dirname, "music")));
-app.use('/images', express.static(path.join(__dirname, "images")));
+// app.use('/images', express.static(path.join(__dirname, "images")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/models", express.static(path.join(__dirname, "faceDetection/model")));
 // app.use('/', (req,res,next)=>{
